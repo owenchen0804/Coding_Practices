c01572833b069fb659aacd0836e8e6b01610add9
owenchen0804 <owenchen0804@gmail.com>
2021-10-04 21:30:15 -0700

    DP I continued

---
+++ src/main/java/DPI/LA99_DictionaryWordI.java
@@ -0,0 +1,29 @@
+package DPI;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LA99_DictionaryWordI {
+    public boolean canBreak(String input, String[] dict) {
+        Set<String> set = new HashSet<>();
+        for (String s : dict) {
+            set.add(s);
+        }
+        //  char[] array = input.toCharArray();
+        boolean[] M = new boolean[input.length() + 1];
+        M[0] = true;
+        for (int i = 1; i < M.length; i++) {
+            // input got built from i = 1 to input.length()
+            for (int j = 0; j < i; j++) {
+                // M[j] 表示左大段通过查表可以得到结果，右边剩下的小段要通过手动查是否在set里面
+                if (M[j] && set.contains(input.substring(j, i))) {  // 必须从j到i，而不是到最后！
+                    M[i] = true;
+                    break;
+                }
+                // M[i] = false;
+                //  逻辑上没错，但是没必要，因为M[]作为new出来的boolean array本身默认值都是false
+            }
+        }
+        return M[input.length()];
+    }
+}
