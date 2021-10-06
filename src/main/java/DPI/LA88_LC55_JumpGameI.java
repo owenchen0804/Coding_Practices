85d423faa4ed73bfa2afc4ba83cc59bdf1efa313
owenchen0804 <owenchen0804@gmail.com>
2021-10-05 20:51:32 -0700

    DP I continued

---
+++ src/main/java/DPI/LA88_LC55_JumpGameI.java
@@ -0,0 +1,42 @@
+package DPI;
+
+public class LA88_LC55_JumpGameI {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        boolean[] dp = new boolean[nums.length];
+        dp[nums.length - 1] = true;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            //  直接靠自己
+            if (i + nums[i] >= nums.length - 1) {
+                dp[i] = true;
+            }
+            else {  // 靠先跳到后面的能够到终点的点
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (dp[j] && i + nums[i] >= j) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+        return dp[0];
+    }
+
+    // Greedy
+    // maxReach是初始值为0，随着i的变化，我们要incremental update maxReach = Math.max(maxReach, i + nums[i])
+    // 当发现存在某个i，且i > maxReach的时候，表明无论如何maxReach也到不了index = i了，就要return false
+
+    public boolean canJumpII(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
--- src/main/java/DPI/LA99_DictionaryWordI.java
+++ src/main/java/DPI/LA99_DictionaryWordI.java
@@ -12,10 +12,12 @@ public class LA99_DictionaryWordI {
         //  char[] array = input.toCharArray();
         boolean[] M = new boolean[input.length() + 1];
         M[0] = true;
+        //  M[i] represents the substring [0, i)
+        //  M[0] represents "", M[1] represents input[0], M[2] => input[0 - 1]
         for (int i = 1; i < M.length; i++) {
             // input got built from i = 1 to input.length()
             for (int j = 0; j < i; j++) {
-                // M[j] 表示左大段通过查表可以得到结果，右边剩下的小段要通过手动查是否在set里面
+                // M[j] 表示左大段, 通过查表可以得到结果，右边剩下的小段要通过手动查是否在set里面
                 if (M[j] && set.contains(input.substring(j, i))) {  // 必须从j到i，而不是到最后！
                     M[i] = true;
                     break;
