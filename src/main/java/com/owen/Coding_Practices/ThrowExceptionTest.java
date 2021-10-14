284a788164fe858bcd50abe7ec1797a339902630
owenchen0804 <owenchen0804@gmail.com>
2021-10-13 20:45:45 -0700

    Throw Exception example

---
+++ src/main/java/com/owen/Coding_Practices/ThrowExceptionTest.java
@@ -0,0 +1,18 @@
+package com.owen.Coding_Practices;
+
+public class ThrowExceptionTest {
+    public static void main(String[] args) {
+        try {
+            int a = 0;
+            System.out.println("a=" + a);
+            int b = 20 / a;
+            System.out.println("Division done.");
+            System.out.println("b=" + b);
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by zero error");
+        } finally {
+            System.out.println("inside the finally block");
+        }
+        System.out.println("after the finally block");
+    }
+}
