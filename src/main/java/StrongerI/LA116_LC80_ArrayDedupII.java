f35250c21e0252db650905c06c3b9192c868b438
owenchen0804 <owenchen0804@gmail.com>
2021-11-19 19:37:43 -0800

    加强练习1 continued

--- src/main/java/StrongerI/LA116_LC80_ArrayDedupII.java
+++ src/main/java/StrongerI/LA116_LC80_ArrayDedupII.java
@@ -1,6 +1,7 @@
 package StrongerI;
 
 public class LA116_LC80_ArrayDedupII {
+    // 有重复也只保留2个一样的，在sorted array的前提下，只要比较fast和slow-2是否相等
     public int removeDuplicates(int[] nums) {
         if (nums.length <= 2) {
             return nums.length;
---
+++ src/main/java/StrongerI/LA117_ArrayDedupIII.java
@@ -0,0 +1,24 @@
+package StrongerI;
+
+public class LA117_ArrayDedupIII {
+    public int compareVersion(String version1, String version2) {
+        int index1 = 0, index2 = 0;
+        while (index1 < version1.length() || index2 < version2.length()) {
+            int ver1 = 0, ver2 = 0;
+            // 算出version1的'.'出现之前的数字
+            while (index1 < version1.length() && version1.charAt(index1) != '.') {
+                ver1 = ver1 * 10 + version1.charAt(index1) - '0';
+                index1++;
+            }
+            while (index2 < version2.length() && version2.charAt(index2) != '.') {
+                ver2 = ver2 * 10 + version2.charAt(index2) - '0';
+                index2++;
+            }
+            if (ver1 < ver2) return -1;
+            if (ver1 > ver2) return 1;
+            index1++;
+            index2++;
+        }
+        return 0;
+    }
+}
