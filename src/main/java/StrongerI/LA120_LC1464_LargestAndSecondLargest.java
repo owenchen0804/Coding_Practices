958506aca2ea42718f5ee3de9727a0225f59ae1d
owenchen0804 <owenchen0804@gmail.com>
2021-11-23 20:35:44 -0800

    Create LA120_LC1464_LargestAndSecondLargest.java

---
+++ src/main/java/StrongerI/LA120_LC1464_LargestAndSecondLargest.java
@@ -0,0 +1,21 @@
+package StrongerI;
+
+public class LA120_LC1464_LargestAndSecondLargest {
+    public int[] largestAndSecond(int[] array) {
+        // Write your solution here
+        int max1 = 0;
+        int max2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max1) {
+                max2 = max1;
+                max1 = array[i];
+            }
+            else if (array[i] > max2) {
+                max2 = array[i];
+            }
+        }
+        return new int[] {max1, max2};
+
+    }
+
+}
