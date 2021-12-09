8c8916fdf995247f8a02917bd90c905cd8a86453
owenchen0804 <owenchen0804@gmail.com>
2021-12-08 17:57:55 -0800

    Update LA180_LC1_2SUM.java

--- src/main/java/StrongerII/LA180_LC1_2SUM.java
+++ src/main/java/StrongerII/LA180_LC1_2SUM.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LA180_LC1_2SUM {
+    // 如果只是boolean返回true or false用hashSet就够了
+    // 但是这里要返回index，所以只能用hashMap来存index还有对应的value
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
