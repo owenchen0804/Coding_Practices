ec0ab2824902ee46afece853d6a2fe0d9e0e813e
owenchen0804 <owenchen0804@gmail.com>
2021-12-15 15:20:38 -0800

    Create MaxPathSumFromLeafToLeaf.java

---
+++ src/main/java/RecursionIII/MaxPathSumFromLeafToLeaf.java
@@ -0,0 +1,29 @@
+package RecursionIII;
+
+import BinaryTree.TreeNode;
+
+public class MaxPathSumFromLeafToLeaf {
+    public int max(TreeNode root) {
+        int[] max = {Integer.MIN_VALUE};
+        helper(root, max);
+        return max[0];
+    }
+
+    private int helper(TreeNode root, int[] max) {
+        if (root == null) return 0;
+        int left = helper(root.left, max);
+        int right = helper(root.right, max);
+
+        if (root.left == null) {
+            return right + root.key;
+        }
+        if (root.right == null) {
+            return left + root.key;
+        }
+        // left & right都有值，就要做取舍
+        // 且此时的root有资格参加max[]的比拼
+        max[0] = Math.max(max[0], left + right + root.key);
+        // 但是往上传递的还是只能是较大的一边
+        return Math.max(left, right) + root.key;
+    }
+}
