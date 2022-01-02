09ce9fdcc9a968466ec8b9490bf207871572ac42
owenchen0804 <owenchen0804@gmail.com>
2022-01-02 09:10:06 -0800

    Update LA272_LC17_CombinationsForTelephonePadI.java

--- src/main/java/DFS_II/LA272_LC17_CombinationsForTelephonePadI.java
+++ src/main/java/DFS_II/LA272_LC17_CombinationsForTelephonePadI.java
@@ -1,5 +1,34 @@
 package DFS_II;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LA272_LC17_CombinationsForTelephonePadI {
-    
+    public String[] combinations(int number) {
+        // Write your solution here
+        List<String> result = new ArrayList<>();
+        char[] numbers = Integer.toString(number).toCharArray(); //  int -> String -> char[]
+        String[] numToChar = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv","wxyz"};
+        StringBuilder sb = new StringBuilder();
+        helper(result, number, numToChar, 0, sb);
+        return result.toArray(new String[result.size()]);
+        // 把list转成array,但是在new的时候需要给出result.size()
+    }
+
+    private void helper(List<String> result, char[] numbers, String[] numToChar, int level, StringBuilder sb) {
+        if (level == numbers.length) {
+            return;
+        }
+        char[] chars = numToChar[numbers[level] - '0'].toCharArray();
+        // 比如2对应的是abc，那么在当前level的chars[]表示的就是abc，三种可能都存在
+        if (chars.length == 0) { // 如果对应的数字是0或者1会出现这情况
+            helper(result, numbers, numToChar, level + 1, sb);
+        }
+        else {
+            for (int i = 0; i < chars.length; i++) {
+                helper(result, numbers, numToChar, level + 1, sb.append(chars[i]));
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
 }
