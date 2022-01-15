24777aa279d140257f2cb8f7a21f17f496d04ebf
owenchen0804 <owenchen0804@gmail.com>
2022-01-15 09:36:56 -0800

    Create LA662_LC126_WordLadderII.java

---
+++ src/main/java/GraphSearchAlgorithmIII/LA662_LC126_WordLadderII.java
@@ -0,0 +1,15 @@
+package GraphSearchAlgorithmIII;
+
+public class LA662_LC126_WordLadderII {
+    /**
+     * High level design: BFS + DFS
+     *
+     * Step 1: use BFS to build graph (adjacency list of each word), as well as calculating distance from beginWord to
+     * each node in the graph (should store minimum distance)
+     *
+     * Step 2: use DFS to traverse and record path from beginWord to endWord with shortest path. We can use distance map
+     * to control every next word.
+     * */
+
+
+}
