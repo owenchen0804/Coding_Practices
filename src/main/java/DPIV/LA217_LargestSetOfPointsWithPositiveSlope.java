2336c3fd0cde9b0757274e6482efd17f59d9ee82
owenchen0804 <owenchen0804@gmail.com>
2022-02-04 11:57:37 -0800

    Update LA217_LargestSetOfPointsWithPositiveSlope.java

--- src/main/java/DPIV/LA217_LargestSetOfPointsWithPositiveSlope.java
+++ src/main/java/DPIV/LA217_LargestSetOfPointsWithPositiveSlope.java
@@ -19,7 +19,22 @@ public class LA217_LargestSetOfPointsWithPositiveSlope {
             }
             return p1.x < p2.x ? -1 : 1;
         });
+        int result = 0;
+        int[] longest = new int[points.length];
+        for (int i = 0; i < longest.length; i++) {
 
-        
+            for (int j = 0; j < i; j++) {
+                if (points[j].y < points[i].y) {
+                    // 取更大的那个作为新的new longest subsequence
+                    longest[i] = Math.max(longest[j], longest[i]);
+                }
+            }
+            longest[i]++;
+            //  如果成功找到j，那么再算上i自己那么Points的个数会+1
+            //  如果直接就是递减序列的话就单独是自己0+1 = 1
+            result = Math.max(result, longest[i]);
+        }
+        return result == 1 ? 0 : result;
+        // result如果还是1，表明最长的subsequence就是自己，说明没有任何两个点能组成正的slope
     }
 }
