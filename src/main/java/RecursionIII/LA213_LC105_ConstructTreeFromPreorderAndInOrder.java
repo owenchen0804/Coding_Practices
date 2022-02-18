d1360c54115e3a90fd8bd3a8fe2615b1e50c66ea
owenchen0804 <owenchen0804@gmail.com>
2022-02-18 11:36:34 -0800

    Update LA213_LC105_ConstructTreeFromPreorderAndInOrder.java

--- src/main/java/RecursionIII/LA213_LC105_ConstructTreeFromPreorderAndInOrder.java
+++ src/main/java/RecursionIII/LA213_LC105_ConstructTreeFromPreorderAndInOrder.java
@@ -10,15 +10,17 @@ public class LA213_LC105_ConstructTreeFromPreorderAndInOrder {
         Map<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             indexMap.put(inorder[i], i);
-            // 记录每个treenode.key作为map的key；
-            //  对应所在的index作为map的value
+            //  记录每个Treenode的值作为map的key；
+            //  对应inorder[]所在的index作为map的value
         }
         return helper(preorder, indexMap, 0, inorder.length - 1, 0, preorder.length - 1);
     }
 
     private TreeNode helper(int[] preorder, Map<Integer, Integer> indexMap, int inLeft, int inRight, int preLeft, int preRight) {
         if (inLeft > inRight) {
-            // 注意这里base case 一定要是inLeft > inRight，不能等于！否则的话当前root的左边只有一个a的话，应该root.left = a的，但是如果可以等于，相当于只有一个元素的时候就return null了，那么就相当于root.left = null了
+            // 注意这里base case 一定要是inLeft > inRight，不能等于！否则的话当前root的左边只有一个a的话，
+            // 应该root.left = a的，但是如果可以等于，相当于只有一个元素的时候就return null了，
+            // 那么就相当于root.left = null了
             return null;
         }
         TreeNode root = new TreeNode(preorder[preLeft]);
