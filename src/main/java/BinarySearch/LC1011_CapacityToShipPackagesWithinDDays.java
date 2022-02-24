8d0b43aef1ea509db093ed8e55d481e3060d5dba
owenchen0804 <owenchen0804@gmail.com>
2022-02-23 19:52:00 -0800

    Update LC1011_CapacityToShipPackagesWithinDDays.java

--- src/main/java/BinarySearch/LC1011_CapacityToShipPackagesWithinDDays.java
+++ src/main/java/BinarySearch/LC1011_CapacityToShipPackagesWithinDDays.java
@@ -10,6 +10,21 @@ public class LC1011_CapacityToShipPackagesWithinDDays {
             sum += wei;
         }
         int left = max, right = sum;
-        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canShip(weights, days, mid)) {
+                // 可以完成的话说明还不够低，可以再低点
+                mid = right - 1;
+            }
+            else {
+                mid = left + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canShip(int[] weights, int days, int cap) {
+        int day = 1; // 因为是从1开始算日子，不是0-indexed
+
     }
 }
