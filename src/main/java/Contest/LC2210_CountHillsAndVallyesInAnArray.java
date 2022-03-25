68bc4ba44c7ad6ce88de463f4505eaf5ab987790
owenchen0804 <owenchen0804@gmail.com>
2022-03-25 14:31:44 -0700

    Update LC2210_CountHillsAndVallyesInAnArray.java

--- src/main/java/Contest/LC2210_CountHillsAndVallyesInAnArray.java
+++ src/main/java/Contest/LC2210_CountHillsAndVallyesInAnArray.java
@@ -1,7 +1,19 @@
 package Contest;
 
 public class LC2210_CountHillsAndVallyesInAnArray {
-    public int countHillValley(int[] nums) {
+
+    //  其实不用下面的解法那么复杂，只需要给一个Left值，跟着i往前走，符合条件的话那么left的值就是这一次的a[i]
+    public int countHillValley(int[] a){
+        int r = 0, left = a[0];
+        for(int i = 1; i < a.length - 1; i++)
+            if(left < a[i] && a[i] > a[i + 1] || left > a[i] && a[i] < a[i + 1]){
+                r++;
+                left = a[i];
+            }
+        return r;
+    }
+
+    public int countHillValleyComplex(int[] nums) {
         int result = 0;
         int start = 0, i = 1;
         while (i < nums.length && nums[i] == nums[start]) {
