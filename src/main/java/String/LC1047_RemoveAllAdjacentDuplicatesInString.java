60929a699c439700feb5f62d0e460252cbef3b6f
owenchen0804 <owenchen0804@gmail.com>
2022-05-30 22:46:14 -0700

    Create LC1047_RemoveAllAdjacentDuplicatesInString.java

---
+++ src/main/java/String/LC1047_RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,26 @@
+package String;
+
+public class LC1047_RemoveAllAdjacentDuplicatesInString {
+    public String removeDuplicates(String s) {
+        //  消消乐，需要模拟一个像stack一样工作的双指针
+        //  这道题不是把所有相同的全部消掉，而是只消除2个！
+        //  不能只靠移动指针，还需要把要保留的部分copy到result array里面！
+        int slow = -1, fast = 0;
+        //  slow includes the result
+        char[] array = s.toCharArray();
+        while (fast < s.length()) {
+            if (slow == -1 || array[slow] != array[fast]) {
+                slow++;
+                array[slow] = array[fast++];
+            }
+            else {  // 就是没有下面这个一直消消乐的while循环了
+                // while (fast + 1 < array.length && array[fast] == array[fast + 1]) {
+                //     fast++; //所有同样的字母全部消掉
+                // }
+                fast++;
+                slow--;
+            }
+        }
+        return new String(array, 0, ++slow);
+    }
+}
