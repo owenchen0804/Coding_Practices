2bb52b73716a74dd3d4381dbf91283d9bc795746
owenchen0804 <owenchen0804@gmail.com>
2022-06-05 08:15:59 -0700

    Update LA253_LC3_LongestSubstringWithoutDuplicate.java

--- src/main/java/String/LA253_LC3_LongestSubstringWithoutDuplicate.java
+++ src/main/java/String/LA253_LC3_LongestSubstringWithoutDuplicate.java
@@ -3,45 +3,24 @@ package String;
 public class LA253_LC3_LongestSubstringWithoutDuplicate {
     public int lengthOfLongestSubstring(String s) {
         int[] map = new int[128];
-        int slow = 0, fast = 0, maxLength = 0;
-        int count = 0;  // count可以针对更多distinct的写法，这里是不许有duplicate，所以不用加count也行，参加👇🏻的solution
+        int slow = 0, fast = 0;
+        int result = 0;
         while (fast < s.length()) {
-            char c1 = s.charAt(fast);
-            if (map[c1] > 0) {
-                count++;
-            }
-            map[c1]++;
-            fast++;
-            while (count > 0) {// it means duplicate
-                char c2 = s.charAt(slow);
-                if (map[c2] > 1) {  // ==1说明正好有一个unique的，>1说明有duplicate了
-                    count--;
-                }
-                map[c2]--;
+            char c = s.charAt(fast);
+            //  只要是fast进来了，那么对应所在的map[c]就需要+1
+            map[c]++;
+            // fast++;
+            while (map[c] > 1) {
+                //  == 1的话正好，说明有且只有一个，否则的话就会进到while循环
+                //  然后一直移动slow，直到l正好也是c，那么把对应的map[]中的值减到1，才能继续动fast
+                char l = s.charAt(slow);
+                map[l]--;
                 slow++;
             }
-            maxLength = Math.max(maxLength, fast - slow);
-        }
-        return maxLength;
-    }
-
-    public int longest(String input) {
-        // Write your solution here
-        int[] chars = new int[128];
-        int slow = 0, fast = 0, longest = 0;
-        while (fast < input.length()) {
-            char f = input.charAt(fast);
-            chars[f]++;
-            while (chars[f] > 1) {  // 如果出现了f character > 1 就一定要把这个character的次数减少到1为止
-                // fast才能继续++
-                char s = input.charAt(slow);
-                chars[s]--;
-                slow++;
-            }
+            result = Math.max(fast - slow + 1, result);
             fast++;
-            longest = Math.max(fast - slow, longest);
         }
-        return longest;
+        return result;
     }
 
     public static void main(String[] args) {
