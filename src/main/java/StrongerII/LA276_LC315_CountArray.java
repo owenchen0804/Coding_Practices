ab522cbecc81eb799a8819fe2d4a21d83d17d616
owenchen0804 <owenchen0804@gmail.com>
2022-06-06 12:02:53 -0700

    Update LA276_LC315_CountArray.java

--- src/main/java/StrongerII/LA276_LC315_CountArray.java
+++ src/main/java/StrongerII/LA276_LC315_CountArray.java
@@ -4,53 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LA276_LC315_CountArray {
-    public List<Integer> countSmaller(int[] nums) {
-        int[] countArray = new int[nums.length];
-        int[] indexArray = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
+    public int[] countArray(int[] array) {
+        // Write your solution here
+        int[] indexArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
             indexArray[i] = i;
         }
-        int[] helperArray = new int[nums.length];
-        mergeSort(nums, indexArray, helperArray, countArray, 0, nums.length - 1);
-        List<Integer> result = new ArrayList<>();
-        for (int i : countArray) {
-            result.add(i);
-        }
-        return result;
+        int[] helper = new int[array.length];
+        int[] countArray = new int[array.length];
+        mergeSort(array, indexArray, countArray, helper, 0, array.length - 1);
+        return countArray;
     }
 
-    private void mergeSort(int[] nums, int[] indexArray, int[] helperArray,
-                           int[] countArray, int left, int right) {
+    private void mergeSort(int[] array, int[] indexArray, int[] countArray, int[] helper, int left, int right) {
         if (left >= right) {
             return;
         }
-
         int mid = left + (right - left) / 2;
-        mergeSort(nums, indexArray, helperArray, countArray, left, mid);
-        mergeSort(nums, indexArray, helperArray, countArray, mid + 1, right);
-        merge(nums, indexArray, helperArray, countArray, left, mid, right);
+        mergeSort(array, indexArray, countArray, helper, left, mid);
+        mergeSort(array, indexArray, countArray, helper, mid + 1, right);
+        merge(array, indexArray, countArray, helper, left, mid, right);
     }
 
-    private void merge(int[] nums, int[] indexArray, int[] helperArray,
-                       int[] countArray, int left, int mid, int right) {
-        copy(indexArray, helperArray, left, right);
-        int l = left, cur = left, r = mid + 1;
+    private void merge(int[]array, int[]indexArray, int[]countArray, int[] helper, int left, int mid, int right) {
+        copyArray(indexArray, helper, left, right);
+        int cur = left;
+        int l = left, r = mid + 1;
         while (l <= mid) {
-            if (r > right || nums[helperArray[l]] <= nums[helperArray[r]]) {
-                // 左边比右边小，需要清算countArray
-                countArray[helperArray[l]] += r - mid - 1;
-                indexArray[cur++] = helperArray[l++];
+            if (r > right || array[helper[l]] <= array[helper[r]]) {
+                countArray[helper[l]] += r - mid - 1; //counterArray存的也都是对于helper[i]这个index有多少比它小的逆序数
+                //  这里是l对应的数该清算了，因为要么r出界了，要么是r对应的值超过了l对应的值
+                //  现在就把l对应的坐标放到当前的indexArray[cur]中
+                indexArray[cur++] = helper[l++];
             }
             else {
-                indexArray[cur++] = helperArray[r++];
+                indexArray[cur++] = helper[r++];  // array[helper[r]]更小，就把它放到cur所对应的indexArray[cur]
             }
         }
     }
 
-    private void copy(int[] indexArray, int[] helperArray, int left, int right) {
+    private void copyArray(int[] indexArray, int[] helper, int left, int right) {
         for (int i = left; i <= right; i++) {
-            helperArray[i] = indexArray[i]; // 记录上一次merge()的index排序的地方
-            //再展开这一次的排序
+            helper[i] = indexArray[i];
         }
     }
 }
