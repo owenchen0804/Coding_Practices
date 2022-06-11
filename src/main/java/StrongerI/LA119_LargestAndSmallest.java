f8b79b9781b3077b98ffd9fc33f5ba183d3061f4
owenchen0804 <owenchen0804@gmail.com>
2022-06-11 08:20:46 -0700

    Update LA119_LargestAndSmallest.java

--- src/main/java/StrongerI/LA119_LargestAndSmallest.java
+++ src/main/java/StrongerI/LA119_LargestAndSmallest.java
@@ -8,7 +8,10 @@ public class LA119_LargestAndSmallest {
             if (array[i] < array[N - 1 - i]) {
                 swap(array, i, N - 1 - i);
             }
-        }// 两两比较且swap，把胜者组放在前半段，败者组放在后半段，再各自找winner/loser
+        }
+        //  两两比较且swap，把胜者组放在前半段，败者组放在后半段，再各自找winner/loser
+        //  这样的比较次数最少，前面分出胜败组是n/2,然后算上下面各比较n/2，一共3n/2
+        //  如果直接O(n)的循环比较，就是2n, > 3n/2
         int largest = array[0];
         for (int i = 1; i < N / 2; i++) {
             largest = Math.max(largest, array[i]);
