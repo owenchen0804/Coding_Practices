e548df318885fc75b1b38465bac22cf9677e3a1d
owenchen0804 <owenchen0804@gmail.com>
2022-08-02 20:09:09 -0700

    Update LC437_Better.java

--- src/main/java/BinaryTreeEnhanced/LC437_Better.java
+++ src/main/java/BinaryTreeEnhanced/LC437_Better.java
@@ -38,12 +38,18 @@ public class LC437_Better {
 
     public static void main(String[] args) {
         LC437_Better testCase = new LC437_Better();
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(-2);
-        TreeNode t3 = new TreeNode(-3);
+        TreeNode t1 = new TreeNode(5);
+        TreeNode t2 = new TreeNode(3);
+        TreeNode t3 = new TreeNode(4);
+        TreeNode t4 = new TreeNode(2);
+        TreeNode t5 = new TreeNode(2);
+        TreeNode t6 = new TreeNode(1);
         t1.left = t2;
         t1.right = t3;
-        System.out.println(testCase.pathSum(t1, -1));
+        t2.left = t4;
+        t2.right = t5;
+        t3.left = t6;
+        System.out.println(testCase.pathSum(t1, 10));
     }
 
 }
