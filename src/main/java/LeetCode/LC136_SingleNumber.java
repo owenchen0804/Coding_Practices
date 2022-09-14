36638663ca38f6969bb9ac1a295c0e69d95cbcd0
owenchen0804 <owenchen0804@gmail.com>
2022-09-14 09:37:10 -0700

    Create LC136_SingleNumber.java

---
+++ src/main/java/LeetCode/LC136_SingleNumber.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class LC136_SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans =0;
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
