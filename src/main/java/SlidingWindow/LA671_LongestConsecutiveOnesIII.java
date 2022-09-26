8efd6266607a18a6331bc70af96847ee430b1d8d
owenchen0804 <owenchen0804@gmail.com>
2022-09-25 17:39:08 -0700

    Create LA671_LongestConsecutiveOnesIII.java

---
+++ src/main/java/SlidingWindow/LA671_LongestConsecutiveOnesIII.java
@@ -0,0 +1,18 @@
+package SlidingWindow;
+
+public class LA671_LongestConsecutiveOnesIII {
+    public int longestConsecutiveOnes(int[] nums, int k) {
+        // Write your solution here
+        int left = 0, right;
+        for (right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                k--;
+            }
+            if (k < 0) {
+                k += 1 - nums[left];
+                left++;
+            }
+        }
+        return right - left;
+    }
+}
