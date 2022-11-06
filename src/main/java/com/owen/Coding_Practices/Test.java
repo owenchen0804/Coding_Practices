ee3d9510ea266b2b406db4cb32b95e460397376a
owenchen0804 <owenchen0804@gmail.com>
2022-11-06 10:04:17 -0800

    Update Test.java

--- src/main/java/com/owen/Coding_Practices/Test.java
+++ src/main/java/com/owen/Coding_Practices/Test.java
@@ -29,38 +29,7 @@ public class Test{
     }
 
     public static void main(String[] args) {
-        Test test = new Test();
-        TreeNode t1 = new TreeNode(0);
-        TreeNode t2 = new TreeNode(1);
-        TreeNode t3 = new TreeNode(2);
-        TreeNode t4 = new TreeNode(0);
-        TreeNode t5 = new TreeNode(0);
-        TreeNode t6 = new TreeNode(0);
-        TreeNode t7 = new TreeNode(0);
-        TreeNode t8 = new TreeNode(1);
-        TreeNode t9 = new TreeNode(1);
-        TreeNode t10 = new TreeNode(1);
-        TreeNode t11 = new TreeNode(1);
-        TreeNode t12 = new TreeNode(2);
-        TreeNode t13 = new TreeNode(2);
-        TreeNode t14 = new TreeNode(2);
-        TreeNode t15 = new TreeNode(2);
-
-        t1.left = t2;
-        t1.right = t3;
-        t2.left = t4;
-        t2.right = t5;
-        t3.left = t6;
-        t3.right = t7;
-        t4.left = t8;
-        t4.right = t9;
-        t5.left = t10;
-        t5.right = t11;
-        t6.left = t12;
-        t6.right = t13;
-        t7.left = t14;
-        t7.right = t15;
-
-        test.reverseOddLevels(t1);
+        long a;
+        //`System.out.print(Long.compare(a, 1));
     }
 }
