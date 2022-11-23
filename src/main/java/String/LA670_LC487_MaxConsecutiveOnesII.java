3f31bd32fd757c95859213d80b4b49a8cd32bcf7
owenchen0804 <owenchen0804@gmail.com>
2022-11-23 14:44:16 -0800

    Update LA670_LC487_MaxConsecutiveOnesII.java

--- src/main/java/String/LA670_LC487_MaxConsecutiveOnesII.java
+++ src/main/java/String/LA670_LC487_MaxConsecutiveOnesII.java
@@ -5,14 +5,17 @@ public class LA670_LC487_MaxConsecutiveOnesII {
         int slow = 0, fast;
         int flag = 1;
         for (fast = 0; fast < nums.length; fast++) {
+            //  只要为0，那么就会让flag--，下不封顶
             if (nums[fast] == 0) {
                 flag--;
             }
+            //  只要小于0，就需要进到这里看看，且只有nums[slow] = 0的时候可以帮助恢复flag
             if (flag < 0) {
                 flag += 1 - nums[slow];
                 slow++;
             }
         }
+        //  由于只要flag为负数都会slow++，所以到最后fast走完，和slow的差值就是答案
         return fast - slow;
     }
 }
