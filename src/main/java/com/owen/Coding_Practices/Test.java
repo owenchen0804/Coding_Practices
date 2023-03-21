a2f69127b6450413bc5d58c84d93089fceb3ac86
owenchen0804 <owenchen0804@gmail.com>
2023-03-21 08:19:22 -0700

    Update Test.java

--- src/main/java/com/owen/Coding_Practices/Test.java
+++ src/main/java/com/owen/Coding_Practices/Test.java
@@ -7,29 +7,66 @@ import LinkedList.*;
 
 public class Test{
 
-    public TreeNode reverseOddLevels(TreeNode root) {
-        return reverseOddLevels(root, 0);
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long result = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int leftIndex = findSmallest(nums, i, lower, upper);
+            int rightIndex = findLargest(nums, i, lower, upper);
+            if (leftIndex == -1 || rightIndex == -1) continue;
+            result += rightIndex - leftIndex + 1;
+        }
+        return result;
     }
 
-    private TreeNode reverseOddLevels(TreeNode root, int height) {
-        if (root.left == null && root.right == null) {
-            return root;
+    private int findSmallest(int[] nums, int i, int lower, int upper) {
+        int left = i + 1, right = nums.length - 1;
+        if (nums[i] + nums[left] > lower || nums[i] + nums[right] < upper) return -1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[i] + nums[mid] >= lower && nums[i] + nums[mid] <= upper) {
+                right = mid;
+            }
+            else if (nums[i] + nums[mid] < lower) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        TreeNode left = reverseOddLevels(root.left, height + 1);
-        TreeNode right = reverseOddLevels(root.right, height + 1);
-        if (height % 2 == 0) {
-            root.left = right;
-            root.right = left;
+        return right;
+    }
+
+    private int findLargest(int[] nums, int i, int lower, int upper) {
+        int left = i + 1, right = nums.length - 1;
+        if (nums[i] + nums[left] > lower || nums[i] + nums[right] < upper) return -1;
+
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[i] + nums[mid] >= lower && nums[i] + nums[mid] <= upper) {
+                left = mid;
+            }
+            else if (nums[i] + nums[mid] < lower) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        else {
-            root.left = left;
-            root.right = right;
-        }
-        return root;
+        if (nums[i] + nums[right] >= lower && nums[i] + nums[right] <= upper) return right;
+        if (nums[i] + nums[left] >= lower && nums[i] + nums[left] <= upper) return left;
+        return -1;
     }
 
     public static void main(String[] args) {
-        long a;
-        //`System.out.print(Long.compare(a, 1));
+
+
+
+
+        Integer[] array = {0,1,4,4,5,7};
+        //System.out.print(Long.compare(a, 1));
+        Arrays.sort(array, (a, b) -> Integer.compare(b, a));
+        System.out.println(array);
     }
 }
